5d810b9646720cdc5849693f2f60f80f80913d96
johns <bboguszewicz@gmail.com>
2014-09-15 16:49:17 +0200

    Update - added serial port connector.

---
+++ SleepGuard/src/com/bb/sleepguard/src/SimpleConnector.java
@@ -0,0 +1,124 @@
+package com.bb.sleepguard.src;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+import javax.sound.sampled.DataLine;
+
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+public class SimpleConnector {
+
+	static SerialPort serialPort;
+	static ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+	static int dataLength = 0;
+
+	public SimpleConnector(String portName) {
+
+		serialPort = new SerialPort(portName);
+		try {
+			serialPort.setParams(SerialPort.BAUDRATE_115200,
+					SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);// Set params. Also you can set
+											// params by this string:
+											// serialPort.setParams(9600, 8, 1,
+											// 0);
+
+			int mask = SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS
+					+ SerialPort.MASK_DSR;// Prepare mask
+			serialPort.setEventsMask(mask);// Set mask
+			serialPort.addEventListener(new SerialPortReader());// Add
+																// SerialPortEventListener
+
+		} catch (SerialPortException ex) {
+			System.out.println(ex);
+		}
+
+	}
+
+	public static String[] listAvailablePorts() {
+
+		String[] portNames = SerialPortList.getPortNames();
+		return portNames;
+
+	}
+
+	public void open() {
+
+		try {
+			serialPort.openPort();
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void close() {
+
+		try {
+			serialPort.closePort();
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void write(byte[] data) {
+		try {
+			serialPort.writeBytes(data);
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public byte[] read() {
+
+		byte[] data = buffer.toByteArray();
+		buffer.reset();
+		dataLength = 0;
+		return data;
+
+	}
+
+	static class SerialPortReader implements SerialPortEventListener {
+
+		public void serialEvent(SerialPortEvent event) {
+			if (event.isRXCHAR()) {// If data is available
+
+				// Read data, if 10 bytes available
+				try {
+					int temp = event.getEventValue();
+					buffer.write(serialPort.readBytes(temp));
+					dataLength+=temp;
+					
+				} catch (SerialPortException | IOException ex) {
+					System.out.println(ex);
+				}
+
+			} else if (event.isCTS()) {// If CTS line has changed state
+				if (event.getEventValue() == 1) {// If line is ON
+					System.out.println("CTS - ON");
+				} else {
+					System.out.println("CTS - OFF");
+				}
+			} else if (event.isDSR()) {// /If DSR line has changed state
+				if (event.getEventValue() == 1) {// If line is ON
+					System.out.println("DSR - ON");
+				} else {
+					System.out.println("DSR - OFF");
+				}
+			}
+		}
+
+	}
+
+}
--- SleepGuard/src/com/bb/sleepguard/src/SleepGuard.java
+++ SleepGuard/src/com/bb/sleepguard/src/SleepGuard.java
@@ -6,15 +6,34 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.UIManager;
+import java.awt.FlowLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JTree;
+
 
 public class SleepGuard extends JFrame {
 
 	private JPanel contentPane;
+	private SimpleConnector connector;
+	private static String[] portNames= SimpleConnector.listAvailablePorts();
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.alee.laf.WebLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -32,11 +51,65 @@ public class SleepGuard extends JFrame {
 	 */
 	public SleepGuard() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(13, 12, 596, 537);
+		contentPane.add(tabbedPane);
+		
+		JChartPanel chartPanel = new JChartPanel();
+		tabbedPane.addTab("Chart", null, chartPanel, null);
+		FlowLayout flowLayout = (FlowLayout) chartPanel.getLayout();
+		flowLayout.setVgap(0);
+		flowLayout.setHgap(0);
+		
+		JTabbedPane tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane_1.setBounds(621, 12, 151, 537);
+		contentPane.add(tabbedPane_1);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane_1.addTab("Commands", null, panel_1, null);
+		panel_1.setLayout(new MigLayout("", "[100px:150px:150px,center]", "[30px:35px:40px][30px:35px:40px][30px:35px:40px][30px:35px:40px][30px:35px:40px][30px:35px:40px][30px:35px:40px]"));
+		
+		JButton btnNewButton = new JButton("Read memory");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_1.add(btnNewButton, "cell 0 0");
+		
+		JButton btnNewButton_1 = new JButton("Erase memory");
+		panel_1.add(btnNewButton_1, "cell 0 1");
+		
+		JButton btnNewButton_2 = new JButton("Start measurement");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel_1.add(btnNewButton_2, "cell 0 2");
+		
+		JButton btnNewButton_3 = new JButton("Stop measurement");
+		panel_1.add(btnNewButton_3, "cell 0 3");
+		
+		JList list = new JList();
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"test", "test2", "test3", "test4", "test5"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		
+		JScrollPane scrollPane = new JScrollPane(list);
+		tabbedPane_1.addTab("Records", null, scrollPane, null);
+		
+		
+		
 	}
-
 }
